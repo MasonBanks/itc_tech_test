f8f62f4594cd11bf19300655a2eaaf685d35ea13
Mason Banks <YokiStation@masons-mbp.home>
2019-09-26 22:19:04 +0100

    create appbar w/ links

--- itc_techtest/src/App.js
+++ itc_techtest/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import * as Material from '@material-ui/core';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import Appbar from './components/appbar/Appbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Appbar />
+      </div>
+    </ Router>
   );
 }
 
---
+++ itc_techtest/src/components/appbar/Appbar.jsx
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import { Route, Link } from 'react-router-dom';
+
+import './Appbar.css'
+import logo from '../../media/logo192.png'
+
+export default class Appbar extends Component {
+  render() {
+    const styles = {
+      barContainer: {
+        height: '93px',
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center"
+      },
+      logo: {
+        height: '93px'
+      },
+      appbar: {
+        width: "100%",
+        height: "93px",
+        backgroundColor: "#41a0db"
+      },
+      links: {
+        fontFamily: "OpenSans",
+        textDecoration: "none",
+        fontSize: "14px",
+        textAlign: "center",
+        color: "#ffffff",
+      },
+      list: {
+        listStyle: "none",
+        display: 'flex',
+        justifyContent: 'space-around',
+        flexFlow: 'row',
+        width: '1000px',
+      },
+      listItem: {
+
+      }
+    }
+    return (
+      <div >
+        <AppBar style={styles.appbar} >
+          <div style={styles.barContainer}>
+            <img src={logo} alt='company logo' style={styles.logo} />
+            <div styles={styles.linkContainer}>
+              <ul style={styles.list}>
+                <li styles={styles.listItem}>
+                  <Link style={styles.links} to="/"><p className='listItem' >Home</p></Link>
+                </li>
+                <li styles={styles.listItem}>
+                  <Link style={styles.links} className='listItem' to="/about/">
+                    <p className='listItem' >Who we are</p>
+                  </Link>
+                </li>
+                <li styles={styles.listItem}>
+                  <Link style={styles.links} className='listItem' to="/users/">
+                    <p className='listItem' >What we do</p>
+                  </Link>
+                </li>
+                <li styles={styles.listItem}>
+                  <Link style={styles.links} className='listItem' to="/users/">
+                    <p className='listItem' >Vacancies</p>
+                  </Link>
+                </li>
+                <li styles={styles.listItem}>
+                  <Link style={styles.links} className='listItem' to="/users/">
+                    <p className='listItem' >News</p>
+                  </Link>
+                </li>
+                <li styles={styles.listItem}>
+                  <Link style={styles.links} className='listItem' to="/users/">
+                    <p className='listItem' >Contact Us</p>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </AppBar>
+      </div>
+    );
+  }
+}
